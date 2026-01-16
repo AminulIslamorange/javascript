554f9961550f5cfdce4833c1f7329bcbaf388d71
AminulIslamorange <aminulislamorange@gmail.com>
2026-01-16 18:57:33 +0600

    first commit

---
+++ convert.js
@@ -0,0 +1,17 @@
+function inceToFeet(ince){
+    const feet=ince/12;
+    return feet;
+
+}
+const aminulHigth=inceToFeet(60);
+// console.log(aminulHigth);
+
+
+
+function milestoKilo(mile){
+    const kilomiter=mile/1.6
+    return kilomiter;
+}
+
+const amardistance=milestoKilo(1);
+console.log(amardistance)
