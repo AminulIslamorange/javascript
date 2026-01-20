6d1e22c0e63093a4ffa548a6685b44ff22eada48
AminulIslamorange <aminulislamorange@gmail.com>
2026-01-20 21:15:31 +0600

    max min

--- duplicate.js
+++ duplicate.js
@@ -1,14 +1,15 @@
-const biriyaniKhur=['mora','jora','khara','juja','mora','abul','babyl','kabol'];
+// const biriyaniKhur=['mora','jora','khara','juja','mora','abul','babyl','kabol'];
 
-function noduplicate(arry){
-    const unique=[]
-    for(man of arry){
-        if(unique.includes(man)===false){
-            unique.push(man)
-        }
-    }
-    return unique;
+// function noduplicate(arry){
+//     const unique=[]
+//     for(man of arry){
+//         if(unique.includes(man)===false){
+//             unique.push(man)
+//         }
+//     }
+//     return unique;
 
 
-}
-const unique=noduplicate(biriyaniKhur)
+// }
+// const unique=noduplicate(biriyaniKhur)
+console.log(Math.round(2.5))
---
+++ max.js
@@ -0,0 +1,5 @@
+const disa=32
+const salman=96
+if(salman> disa){
+    console.log('salman winner')
+}
